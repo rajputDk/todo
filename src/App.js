bba69e1eea358f9a708ae798e11470c5e333177a
deepak singh <dhonideepaksingh123@gmail.com>
2023-02-01 13:17:05 +0530

    new todo add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [input,setInput]=useState(" ")
+  const [upDate,setUpDate]=useState([
+    
+  ])
+  const addTodo =(e)=>{
+    e.preventDefault();
+    const user={
+      input, 
+    }
+    setUpDate([...upDate,user])
+    setInput("")
+  };
   return (
     <div>
-      <h1>App</h1>
+     <div className='main-container'> 
+      <div className='heading'>
+      <h1>To-Do List</h1>
+      </div>
+       <input type="text"
+        id="in"
+         placeholder='Add Here'
+         value={input} 
+         onChange={(e)=> setInput(e.target.value)}/>
+       <button className='btn' onClick={addTodo}>Add</button>
+       <div className='list'>
+       {
+          upDate.map(user=>{
+            return(
+              <h1 className='head'>{user.input}</h1>
+            )
+          })
+        }
+     
+       </div>
+     </div> 
     </div>
   );
 }
 
 export default App;
+       
