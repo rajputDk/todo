b15cdb4236550d2c666d861f52b3c0aaf6cbef96
deepak singh <dhonideepaksingh123@gmail.com>
2023-02-02 15:34:24 +0530

    todo-complete

--- src/App.js
+++ src/App.js
@@ -1,48 +1,102 @@
-
-import React, { useState } from 'react'
-// import { v4 } from 'uuid';
-import './App.css'; 
-
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [input,setInput]=useState()
-  const [upDate,setUpDate]=useState([])
-  const addTodo =(e)=>{
-    e.preventDefault();
-    setUpDate([...upDate,{input}])
-    setInput("")
-  };
-  const deleteItem = (index)=>{
-   upDate.splice(index,1)
-   setUpDate([...upDate])
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+  const [editId, setEditId] = useState();
+  const [toggleSubmit, setToggleSubmit] = useState(true);
+
+  //adding Task
+  function AddTask(e) {
+    if (task !== "") {
+      setTaskList([...taskList, { task, id: `${task}-${Date.now()}` }]);
+      setTask("");
+    }
+    if (editId) {
+      const editTask = taskList.find((i) => i.id === editId);
+      const updatedTasks = taskList.map((t) =>
+        t.id === editTask.id
+          ? (t = { id: t.id, task })
+          : { id: t.id, task: t.task }
+      );
+      setTaskList(updatedTasks);
+      setEditId("");
+      return setToggleSubmit(true);
+    }
   }
+  const deleteTodo = (id) => {
+    const newList = taskList.filter((t) => t.id !== id);
+    setTaskList([...newList]);
+  };
+  // Editing Task
+  const handleEdit = (id) => {
+    const editTask = taskList.find((i) => i.id === id);
+    setTask(editTask.task);
+    setEditId(id);
+    setToggleSubmit(false);
+  };
+
+  const handleKeydown = (e) => {
+    if (e.keyCode === 13) {
+      AddTask(e);
+    }
+  };
   return (
     <div>
-     <div className='main-container'> 
-      <div className='heading'>
-      <h1>To-Do List</h1>
+      <div className="main-container">
+        <div className="heading">
+          <h1>To-Do List</h1>
+        </div>
+        <div onKeyDown={handleKeydown}>
+          <input
+            type="text"
+            id="in"
+            placeholder="Add Here"
+            value={task}
+            onChange={(e) => {
+              setTask(e.target.value);
+            }}
+          />
+          {toggleSubmit ? (
+            <button className="btn" onClick={AddTask}>
+              Add
+            </button>
+          ) : (
+            <button className="btn" onClick={AddTask}>
+              Update
+            </button>
+          )}
+        </div>
+        <div className="list">
+          {taskList.map((task) => {
+            return (
+              <>
+                {" "}
+                <span className="htt" key={task.id}>
+                  {task.task}
+                  <div className="bt">
+                    <button
+                      className="btn1"
+                      onClick={() => deleteTodo(task.id)}
+                    >
+                      Delete
+                    </button>
+                    <button
+                      className="btn2"
+                      onClick={() => handleEdit(task.id)}
+                    >
+                      Edit
+                    </button>
+                  </div>
+                </span>
+              </>
+            );
+          })}
+        </div>
       </div>
-       <input type="text"
-        id="in"
-         placeholder='Add Here'
-         value={input} 
-         onChange={(e)=> setInput(e.target.value)}/>
-       <button className='btn' onClick={addTodo}>Add</button>
-       <div className='list'>
-       {
-          upDate.map((user,index)=>{
-            return(
-              <h1 className='head'>{user.input}
-              <span className='btn2' onClick={()=>deleteItem(index)}>Delete</span></h1>
-            )
-          })
-        }
-     
-       </div>
-     </div> 
     </div>
   );
 }
 
 export default App;
-       
